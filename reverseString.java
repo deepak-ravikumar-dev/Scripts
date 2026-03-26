1b584daff692dd347ecd1483a1cf19cd2b089cff
Deepak .R <deepakravikumar04@gmail.com>
2026-03-26 04:18:55 +0000

    Add reverseString.java

---
+++ reverseString.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class reverseString {
+    public static void main(String[] args) {
+        String str = "krishita";
+        char[] arr = str.toCharArray();
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            char temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+        String reversed = new String(arr);
+        System.out.println(reversed);
+    }
+}
